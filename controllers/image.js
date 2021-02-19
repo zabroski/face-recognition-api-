fc96d24b7ed93a8476edb194640d348dff58c5e7
Issouf Zabre <sheridanzabre@gmail.com>
2021-02-18 21:47:37 -0500

    last commit

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,20 @@
+const clarifai = require('clarifai');
+const { handleProfileGet } = require('./profile');
+
+const app = new clarifai.App ({
+    apiKey: '8b5edfe3537c4a61acb3164a3ab24492'
+  });
+
+const handleApicall = (req, res) => {
+    app.models
+    .predict(clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+        res.json(data);
+    })
+    .catch(err => res.status(400).json('unable to work with API'))
+}
+
+
 const handleImage = (req, res, db) => {
     const {id} = req.body;
     db('users').where('id', '=', id)
@@ -8,6 +25,8 @@ const handleImage = (req, res, db) => {
     })
     .catch(err => res.status(400).json('unable to get entries'))
 }
+
 module.exports = {
-    handleImage: handleImage
+    handleImage: handleImage,
+    handleApicall: handleProfileGet
  }
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const port = 3000;
 const bodyParser = require('body-parser');
@@ -8,11 +9,6 @@ app.use(bodyParser.json())
 app.use(cors());
 const Knex = require('Knex');
 const nodemon = require('nodemon');
-
-
-const register = require('./controllers/register');
-const signin = require('./controllers/signin');
-const profile = require('./controllers/handleProfileGet')
 
 
 const db = Knex({
@@ -30,12 +26,68 @@ app.get('/', (req, res) => {
 })
 
 
-app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)})
+app.post('/signin', (req, res) => {
+    
+    db.select('email', 'hash').from('login')
+    .where('email', '=', req.body.email)
+    .then(data => {
+       const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+       if(isValid) {
+           return db.select('*').from('users')
+           .where('email', '=', req.body.email)
+           .then(user => {
+               res.json(user[0])
+           })
+           .catch(err => res.status(400).json('unable to get user'))
+       } else {
+           res.status(400).json('wrong credentials')
+       }
+    })
+    .catch(err => res.status(400).json('Wrong credentials'))
+})
 
-app.post('/register', (req, res) =>{register.handleRegister(req, res, db, bcrypt)});
+
+app.post('/register', (req, res) => {
+    const { email, name, password } = req.body;
+    const hash = bcrypt.hashSync(password);
+    db.transaction(trx => {
+        trx.insert({
+            hash: hash,
+            email: email
+        })
+        .into('login')
+        .returning('email')
+        .then(loginEmail => {
+            return trx('users')
+            .returning('*')
+            .insert({
+                email: loginEmail[0],
+                name: name,
+                joined: new Date
+            })
+            .then(user => {
+                res.json(user[0]);
+            })
+        })
+        .then(trx.commit)
+        .catch(trx.rollback)
+    })    
+        .catch(err => res.status(400).json('unable to register'));
+});
 
 
-app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db, bcrypt)})
+app.get('/profile/:id', (req, res) => {
+    const {id} = req.params;
+    db.select('*').from('users').where({id})
+    .then(user => {
+        if(user.length) {
+            res.json(user[0])
+        } else {
+            res.status(400).json('Not found')
+        }
+    })
+    .catch(err => res.status(400).json('error getting user'))
+})
 
 
 app.put('/image', (req, res) => {
