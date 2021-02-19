e817a68f00087ef2dbc0327344c25f3bf96bc7e1
Issouf Zabre <sheridanzabre@gmail.com>
2021-02-18 22:32:04 -0500

    fixed port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 
 const express = require('express');
-const port = 3000;
+// const port = 3000;
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const bcrypt = require('bcrypt-nodejs');
