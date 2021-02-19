3f235d5f40140b0c596a71099cbfb069fc00a9e6
Issouf Zabre <sheridanzabre@gmail.com>
2021-02-19 00:07:54 -0500

    heroku

--- server.js
+++ server.js
@@ -103,14 +103,15 @@ app.put('/image', (req, res) => {
 })
 
 
-// app.listen( process.env.PORTR || 3000, () => {
-//     console.log(`app is running on port ${process.env.PORT}`)
-// })
-
-
-app.listen(3000, () => {
-    console.log(`app is running on port ${port}`)
+app.listen(
+    process.env.PORT || 3000, () => {
+    console.log(`app is running on port ${process.env.PORT}`)
 })
+
+
+// app.listen(3000, () => {
+//     console.log(`app is running on port ${port}`)
+// })
 
 
 /*
